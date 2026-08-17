4895d77b808f5708843082a0872cc14bca3ed10c
Kio-pon <175557347+Kio-pon@users.noreply.github.com>
2026-08-17 10:03:53 +0500

    Add CA: files under 15MB (95 files, 78M)

---
+++ CA/Lab/led.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module leds(
+    input clk,
+    input rst,
+    input [31:0] writeData,
+    input writeEnable,
+    input readEnable,
+    input [29:0] memAddress,
+    
+    output reg  [31:0] readData = 0, // not to be read       
+    output reg [15:0] leds
+    );
+    
+    
+   
+endmodule
+
+
